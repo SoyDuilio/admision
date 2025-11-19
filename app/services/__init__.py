778057e72781ef3c373e2b38f7dc08101fc53fff
SoyDuilio <cramadalsuarez@gmail.com>
2025-11-19 13:54:22 -0500

    poi
    w4

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,7 +4,7 @@ Ubicación: app/services/__init__.py
 """
 
 # GENERACIÓN DE PDFs
-from app.services.pdf_generator_v2 import generar_hoja_respuestas_v2 as generar_hoja_respuestas_pdf
+from app.services.pdf_generator_v3 import generar_hoja_respuestas_v3 as generar_hoja_respuestas_pdf
 
 # PROCESAMIENTO DE IMÁGENES
 from app.services.vision_service_v2 import (  # ← CAMBIO AQUÍ
@@ -14,7 +14,7 @@ from app.services.vision_service_v2 import (  # ← CAMBIO AQUÍ
     extraer_con_google_vision
 )
 
-from app.services.image_preprocessor import ImagePreprocessor
+#from app.services.image_preprocessor_v2 import ImagePreprocessorV2
 
 # VALIDACIÓN Y CALIFICACIÓN
 from app.services.validacion import validar_codigos
